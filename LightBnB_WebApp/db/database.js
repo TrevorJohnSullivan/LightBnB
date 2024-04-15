4865e0130f512eeaf6869464d5b778f13cf0da10
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-04-15 16:05:02 -0230

    test

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -77,19 +77,28 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
+  const query = `
+    SELECT reservations.*, 
+           properties.*, 
+           avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
 
   return pool
-  .query('SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating FROM reservations JOIN properties ON reservations.property_id = properties.id JOIN property_reviews ON properties.id = property_reviews.property_id WHERE reservations.guest_id = $1 GROUP BY properties.id, reservations.id ORDER BY reservations.start_date LIMIT 10', [guest_id])
-  .then((result) => {
-    if (result.rows.length === 0) {
-      return null;
-    }
-    return result.rows[0];
-  })
-  .catch((err) => {
-    console.log(err.message);
-    return null;
-  });
+    .query(query, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      throw err;
+    });
 };
 
 /// Properties
